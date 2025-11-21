ef24ba6ce5268bed518e907180fdcdde833c0875
Rhayssa <rsb6@cesar.school>
2025-11-21 20:24:57 -0300

    Tirando caracteres desnecessários para deixar o código mais limpo

--- cadastro_fornecedora.py
+++ cadastro_fornecedora.py
@@ -1,11 +1,11 @@
-import os, json, bcrypt
+import os, json, bcrypt 
 from utils import validar_cnpj
 from pathlib import Path
 
 ROOT_FOLDER = Path(__file__).parent
 INFO_EMPRESAS_FORNECEDORAS = ROOT_FOLDER / 'banco_de_dados' / 'info_empresas_fornecedoras.json'
 
-# -------------------- CARREGAR / SALVAR --------------------
+#CARREGAR / SALVAR 
 
 def carregar_empresas():
     if os.path.exists(INFO_EMPRESAS_FORNECEDORAS):
@@ -21,38 +21,38 @@ def salvar_empresas(empresas):
         json.dump(empresas, f, indent=4, ensure_ascii=False)
 
 
-# -------------------- SISTEMA --------------------
+# SISTEMA 
 
 def sistema_ecomatch():
     empresas = carregar_empresas()
 
     while True:
-        print("\n=== Bem-vindo ao Sistema Eco-Match! ===")
+        print(" === Bem-vindo ao Sistema Eco-Match! ===")
         print("1. Cadastrar Empresa")
         print("2. Validar Cadastro")
         print("3. Listar empresas")
         print("4. Sair")
         opcao = input("Escolha uma opção: ")
 
-        # ---------------- CADASTRAR EMPRESA -------------------
+        # CADASTRO DE EMPRESAS 
         if opcao == "1":
-            print("\n=== CADASTRO DE EMPRESA ===")
+            print("=== CADASTRO DE EMPRESA ===")
             nome = input("Informe o nome da empresa: ")
             cnpj = input("Informe o CNPJ: ")
 
             if not validar_cnpj(cnpj):
-                print("❌ CNPJ inválido! Tente novamente.")
+                print(" CNPJ inválido! Tente novamente.")
                 continue
 
             if cnpj in empresas:
-                print("❌ CNPJ já cadastrado!")
+                print(" CNPJ já cadastrado!")
                 continue
 
             email = input("Informe o e-mail: ")
             telefone = input("Informe o telefone de contato: ")
             senha_plana = input("Senha: ")
 
-            # ---- CRIPTOGRAFAR SENHA ----
+            # CRIPTOGRAFAR SENHA 
             senha_hash = bcrypt.hashpw(senha_plana.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             empresas[cnpj] = {
@@ -64,42 +64,42 @@ def sistema_ecomatch():
 
             salvar_empresas(empresas)
             empresas = carregar_empresas()
-            print("✅ Empresa cadastrada com sucesso!")
+            print(" Empresa cadastrada com sucesso!")
 
-        # ---------------- VALIDAR CADASTRO -------------------
+        # VALIDAÇÃO CADASTRO 
         elif opcao == "2":
-            print("\n=== VALIDAR CADASTRO ===")
+            print("=== VALIDAR CADASTRO ===")
             cnpj = input("CNPJ: ")
             senha = input("Senha: ")
 
             if cnpj not in empresas:
-                print("❌ CNPJ ou senha incorretos.")
+                print(" CNPJ ou senha incorretos.")
                 continue
 
             senha_hash_salva = empresas[cnpj]["senha"].encode('utf-8')
 
-            # ---- VERIFICAR SENHA CRIPTOGRAFADA ----
+            # VERIFICAR SENHA CRIPTOGRAFADA 
             if bcrypt.checkpw(senha.encode('utf-8'), senha_hash_salva):
-                print(f"✅ O seu cadastro está validado com sucesso! Bem-vindo(a), {empresas[cnpj]['nome']}.")
+                print(f" O seu cadastro está validado com sucesso! Bem-vindo(a), {empresas[cnpj]['nome']}.")
             else:
-                print("❌ CNPJ ou senha incorretos.")
+                print(" CNPJ ou senha incorretos.")
 
-        # ---------------- LISTAR EMPRESAS -------------------
+        # LISTAR EMPRESAS 
         elif opcao == "3":
-            print("\n=== EMPRESAS CADASTRADAS ===")
+            print("=== EMPRESAS CADASTRADAS ===")
             if not empresas:
                 print("Nenhuma empresa cadastrada.")
             else:
                 for cnpj, dados in empresas.items():
                     print(f"{dados['nome']} - {cnpj} - {dados['email']} - {dados['telefone']}")
 
-        # ---------------- SAIR -------------------
+        #  SAIR 
         elif opcao == "4":
             print("Encerrando o sistema... ")
             break
 
         else:
-            print("❌ Opção inválida. Tente novamente.")
+            print(" Opção inválida. Tente novamente.")
 
 
 if __name__ == "__main__":
